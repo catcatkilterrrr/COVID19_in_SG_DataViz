eed0e0182eddcbd72f684638701a014a5e9062d5
ctay <ctay@sva.edu>
2020-04-12 19:17:00 +0800

    test

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 // import * as THREE from '/node_modules/three/build/three.js';
 // var THREE = require('/node_modules/three/build/three');
 //import {Patients} from './Patient.js';
-// import {OrbitControls} from '/node_modules/three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
 // import { EffectComposer } from '/node_modules/three/examples/jsm/postprocessing/EffectComposer.js';
 // import { RenderPass } from '/node_modules/three/examples/jsm/postprocessing/RenderPass.js';
 // import { UnrealBloomPass } from '/node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js';
